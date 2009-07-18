1f7482a8d907384b22486db74229fa911e1e9761
torbjorn.maro <torbjorn.maro@3bff01fa-6bc3-11de-ae5f-3f045535ef68>
2009-07-18 16:58:30 +0000

    Added tests for the League class. 72% test coverage.

--- Fussball.SimplePointsSystem.Test/LeagueContext.cs
+++ Fussball.SimplePointsSystem.Test/LeagueContext.cs
@@ -16,9 +16,12 @@ namespace Fussball.SimplePointsSystem.Test
 
         protected Action<int> Given_the_number_of_players_is = (num) =>
         {
-            for (int i = 0; i < num; i++)
+            for (int i = 1; i <= num; i++)
                 players.Add(new Player(i.ToString()));
         };
+
+        protected Func<int, Player> get_player = (index) =>
+            league.Players.GetByName(index.ToString());
 
         protected Action<int> When_generating_a_leage_of_size = (size) =>
         {
