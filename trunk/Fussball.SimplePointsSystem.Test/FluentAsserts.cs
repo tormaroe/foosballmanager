1e53aae2b3f3817057d1cf111fa4529c760164aa
torbjorn.maro <torbjorn.maro@3bff01fa-6bc3-11de-ae5f-3f045535ef68>
2009-07-18 09:30:38 +0000

    Starting to add tests to Fussball.SimplePointsSystem. Added Lib folder with NUnit and Moq. Added tests for producing XML from Players collection. 5% test coverage of Fussball.SimplePointsSystem

---
+++ trunk/Fussball.SimplePointsSystem.Test/FluentAsserts.cs
@@ -0,0 +1,207 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using System.Collections;
+
+namespace Fussball.SimplePointsSystem.Test
+{
+    public static class FluentAsserts
+    {
+        public static T should_satisfy<T>(this T @object, Predicate<T> condition)
+        {
+            Assert.IsTrue(condition(@object));
+            return @object;
+        }
+
+        public static T should_satisfy<T>(this T @object, Predicate<T> condition, string failMessage)
+        {
+            Assert.IsTrue(condition(@object), failMessage);
+            return @object;
+        }
+
+        public static void should_be_false(this bool condition)
+        {
+            Assert.IsFalse(condition);
+        }
+
+        public static void should_be_true(this bool condition)
+        {
+            Assert.IsTrue(condition);
+        }
+
+        public static string should_contain(this string actual, string expected)
+        {
+            Assert.IsTrue(actual.Contains(expected),
+                    String.Format("\"{0}\" does not contain \"{1}\".", actual, expected));
+            return actual;
+        }
+
+        /// <summary>
+        /// Same as should_equal
+        /// </summary>
+        public static T should_be<T>(this T actual, object expected)
+        {
+            return should_equal(actual, expected);
+        }
+
+        public static T should_equal<T>(this T actual, object expected)
+        {
+            Assert.AreEqual(expected, actual);
+            return actual;
+        }
+
+        public static T should_not_equal<T>(this T actual, object expected)
+        {
+            Assert.AreNotEqual(expected, actual);
+            return actual;
+        }
+
+        public static void should_be_null(this object @object)
+        {
+            Assert.IsNull(@object);
+        }
+
+        public static T should_not_be_null<T>(this T @object)
+        {
+            Assert.IsNotNull(@object);
+            return @object;
+        }
+
+        public static T should_be_same_as<T>(this T actual, object expected)
+        {
+            Assert.AreSame(expected, actual);
+            return actual;
+        }
+
+        public static T should_not_be_same_as<T>(this T actual, object expected)
+        {
+            Assert.AreNotSame(expected, actual);
+            return actual;
+        }
+
+        public static object should_be_of_type(this object actual, Type expected)
+        {
+            Assert.IsAssignableFrom(expected, actual);
+            return actual;
+        }
+
+        public static TExpected should_be_of_type<TExpected>(this object actual)
+        {
+            Assert.IsAssignableFrom(typeof(TExpected), actual);
+            return (TExpected)actual;
+        }
+
+        public static object should_be_of_exact_type(this object actual, Type expected)
+        {
+            should_not_be_null(actual);
+            Assert.AreEqual(expected, actual.GetType());
+            return actual;
+        }
+
+        public static TExpected should_be_of_exact_type<TExpected>(this object actual)
+        {
+            return (TExpected)should_be_of_exact_type(actual, typeof(TExpected));
+        }
+
+        public static object Should_not_be_of_type(this object actual, Type expected)
+        {
+            Assert.IsNotAssignableFrom(expected, actual);
+            return actual;
+        }
+
+        public static object should_not_be_of_type<TExpected>(this object actual)
+        {
+            Assert.IsNotAssignableFrom(typeof(TExpected), actual);
+            return actual;
+        }
+
+        public static IEnumerable should_contain(this IEnumerable collection, object expected)
+        {
+            Assert.Contains(expected, collection.Cast<object>().ToList());
+            return collection;
+        }
+
+        public static IEnumerable should_not_contain(this IEnumerable collection, object expected)
+        {
+            Assert.IsFalse(collection.Cast<object>().Contains(expected),
+                    "Collection contains the value.");
+            return collection;
+        }
+
+        public static void should_be_empty(this string @string)
+        {
+            Assert.IsEmpty(@string);
+        }
+
+        public static string should_not_be_empty(this string @string)
+        {
+            Assert.IsNotEmpty(@string);
+            return @string;
+        }
+
+        public static void should_be_empty(this IEnumerable collection)
+        {
+            Assert.IsEmpty(collection.Cast<object>().ToList());
+        }
+
+        public static IEnumerable should_not_be_empty(this IEnumerable collection)
+        {
+            Assert.IsNotEmpty(collection.Cast<object>().ToList());
+            return collection;
+        }
+
+        public static void ShouldNotContain(this string actual, string expected)
+        {
+            Assert.IsFalse(actual.Contains(expected),
+                    String.Format("\"{0}\" contains \"{1}\".", actual, expected));
+        }
+
+        public static void should_contain_error_message(this Exception exception, string expected)
+        {
+            should_contain(exception.Message, expected);
+        }
+
+        public static Exception should_be_thrown_by(this Type exceptionType, Action actionThatThrows)
+        {
+            Exception exception = actionThatThrows.GetException();
+
+            Assert.IsNotNull(exception);
+            Assert.AreEqual(exceptionType, exception.GetType());
+
+            return exception;
+        }
+
+        public static Exception should_be_thrown_by(this Type exceptionType, Action actionThatThrows,
+                Predicate<Exception> exceptionCondition)
+        {
+            Exception exception = actionThatThrows.GetException();
+
+            Assert.IsNotNull(exception);
+            Assert.AreEqual(exceptionType, exception.GetType());
+
+            if (!exceptionCondition(exception))
+                throw new ArgumentException("Exception throw did not satisfy the condition.");
+
+            return exception;
+        }
+
+        public static Exception GetException(this Action actionThatThrows)
+        {
+            Exception exception = null;
+
+            try
+            {
+                actionThatThrows();
+            }
+            catch (Exception e)
+            {
+                exception = e;
+            }
+
+            return exception;
+        }
+
+    }
+}
---
+++ trunk/Fussball.SimplePointsSystem.Test/PlayersToXML.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Fussball.SimplePointsSystem.Test
+{
+    [TestFixture]
+    public class PlayersToXML
+    {
+        [Test]
+        public void Should_create_empty_XML_for_empty_collection()
+        {   
+            Given_an_empty_players_collection();
+            When_making_XML_for_players();
+            xml.should_equal("<players></players>");
+        }
+
+        [Test]
+        public void Should_create_well_known_XML_for_known_person()
+        {
+            Given_a_players_collection_with(knownPerson);
+            When_making_XML_for_players();
+            xml.should_equal(
+                "<players>"
+                + "<player id=\"" + knownPerson.Id + "\">"
+                + "<singleslost>" + knownPerson.SinglesLost + "</singleslost>"
+                + "<singleswon>" + knownPerson.SinglesWon + "</singleswon>"
+                + "<doubleslost>" + knownPerson.DoublesLost + "</doubleslost>"
+                + "<doubleswon>" + knownPerson.DoublesWon + "</doubleswon>"
+                + "<points>" + knownPerson.Points + "</points>"
+                + "<leaguepoints>" + knownPerson.LeaguePoints + "</leaguepoints>"
+                + "<leaguematched>" + knownPerson.LeagueMatchesPlayed + "</leaguematched>"
+                + "<name>" + knownPerson.Name + "</name>"
+                +"</player>" 
+                + "</players>");
+        }
+
+        [Test]
+        public void Should_create_XML_for_collection_with_multiple_persons()
+        {
+            Given_a_players_collection_with_10_persons();
+            When_making_XML_for_players();
+            XML_should_contain_name_elements_for_all_10_people();
+        }
+
+        static Action Given_an_empty_players_collection = () =>
+            players = new Players();
+
+        Action<Player> Given_a_players_collection_with = (person) =>
+        {
+            Given_an_empty_players_collection();
+            players.Add(person);
+        };
+
+        Action Given_a_players_collection_with_10_persons = () =>
+        {
+            Given_an_empty_players_collection();
+            ten_names.ForEach(name => players.Add(new Player(name)));
+        };
+
+        Action When_making_XML_for_players = () =>
+            xml = PlayersXML.ToXml(players);
+
+        Action XML_should_contain_name_elements_for_all_10_people = () =>
+            ten_names.ForEach(name => 
+                xml.should_contain(String.Format("<name>{0}</name>", name)));       
+
+        static Players players;
+        static Player knownPerson = new Player("Johnny");
+        static List<string> ten_names = (new[] { "Arne", "Bob", "Charley", "David", "Edward", "Frank", "Garth", "Henry", "Ivan", "Job" }).ToList();
+        static string xml;
+    }
+}
