4de63d83792f51224c2f8e8cdda1c463a2c4e1bc
torbjorn.maro <torbjorn.maro@3bff01fa-6bc3-11de-ae5f-3f045535ef68>
2009-07-18 16:22:43 +0000

    Added tests for Leage generation.

---
+++ trunk/Fussball.SimplePointsSystem.Test/LeagueContext.cs
@@ -0,0 +1,29 @@
+using System;
+using NUnit.Framework;
+
+namespace Fussball.SimplePointsSystem.Test
+{
+    public class LeagueContext
+    {
+        protected static Players players;
+        protected static League league;
+
+        [SetUp]
+        public void SetUp()
+        {
+            players = new Players();
+        }
+
+        protected Action<int> Given_the_number_of_players_is = (num) =>
+        {
+            for (int i = 0; i < num; i++)
+                players.Add(new Player(i.ToString()));
+        };
+
+        protected Action<int> When_generating_a_leage_of_size = (size) =>
+        {
+            league = new League(new LeagueMatches(), players);
+            league.GenerateMatches(size);
+        };
+    }
+}
---
+++ trunk/Fussball.SimplePointsSystem.Test/LeagueGenerationSpecification.cs
@@ -0,0 +1,45 @@
+using System;
+using NUnit.Framework;
+
+namespace Fussball.SimplePointsSystem.Test
+{
+    [TestFixture]
+    public class LeagueGenerationSpecification : LeagueContext
+    {
+        [Test]
+        public void Two_players_and_minimum_league()
+        {
+            Given_the_number_of_players_is(2);
+            When_generating_a_leage_of_size(1);
+            Then_the_number_of_matches_should_be(2);
+        }
+
+        [Test]
+        public void Two_players_and_really_big_league()
+        {
+            Given_the_number_of_players_is(2);
+            When_generating_a_leage_of_size(10);
+            Then_the_number_of_matches_should_be(20);
+        }
+
+        [Test]
+        public void Three_players_and_minimum_league()
+        {
+            Given_the_number_of_players_is(3);
+            When_generating_a_leage_of_size(1);
+            Then_the_number_of_matches_should_be(6);
+        }
+
+        [Test]
+        public void Five_players_and_double_league()
+        {
+            Given_the_number_of_players_is(5);
+            When_generating_a_leage_of_size(2);
+            Then_the_number_of_matches_should_be(40);
+        }
+       
+        Action<int> Then_the_number_of_matches_should_be = (num) =>
+            league.Matches.AllMatches.Count.should_be(num);
+
+    }
+}
